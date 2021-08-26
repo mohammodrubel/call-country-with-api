42599039f32a28eba9d783a0da8cbd56ac4db1e6
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-27 03:58:51 +0600

    Add files via upload

---
+++ call country with api/js/app.js
@@ -0,0 +1,31 @@
+const countryApi = ()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(Response => Response.json())
+    .then(contry => internationalConutry(contry))
+}
+countryApi()
+
+// const internationalConutry= (contry)=>{
+//     console.log(contry)
+//     const mainContainer = document.getElementById('mainContainer')
+//     for (const singleCountry of contry){
+//         // console.log(singleCountry.name)
+//         const p = document.createElement('p')
+//         p.innerText = singleCountry.name
+//         mainContainer.appendChild(p)
+//     }
+
+// }
+// NOW IMPLEMENT FOREACH 
+const internationalConutry = (contry)=>{
+    console.log(contry)
+    const mainContainer = document.getElementById('mainContainer')
+    contry.forEach (worldCountry =>{
+        // console.log(worldCountry.name)
+        const p = document.createElement('p')
+        p.classList.add('perp')
+        p.innerText = worldCountry.name
+        mainContainer.appendChild(p)
+
+    })
+}
